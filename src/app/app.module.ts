d00679b5f5cc77f278f09713ce5265f5e256ae90
Brunoss21 <80181592+Brunoss21@users.noreply.github.com>
2023-03-29 15:42:54 -0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RootComponent } from './root/root.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  imports: [CommonModule, BrowserModule],
+  declarations: [],
+  bootstrap: [
+    RootComponent,
+    HeaderComponent,
+    ContentComponent,
+    FooterComponent,
+  ],
+})
+export class AppModule {}
